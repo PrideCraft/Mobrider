a03064435376fe96cc53f1d9397c27dc3d5c4c2b
Notarin <424c414e4b@gmail.com>
2022-10-23 17:08:35 -0400

    moved events to main class

--- src/main/java/me/notarin/mobrider/Mobrider.java
+++ src/main/java/me/notarin/mobrider/Mobrider.java
@@ -8,16 +8,32 @@ public final class Mobrider extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        registerEvents();
-        getServer().getConsoleSender().sendMessage("§5MOBRIDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
-    }
-
-    private void registerEvents() {
         Bukkit.getPluginManager().registerEvents(new Events(), this);
+        getServer().getConsoleSender().sendMessage("§5MOBRIDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public class Events implements Listener {
+        @EventHandler
+        public void onInteract(PlayerInteractEntityEvent e) {
+            Player p = e.getPlayer();
+            Entity en = e.getRightClicked();
+            if(!(en instanceof ArmorStand) && !(en instanceof Player)) {
+                en.addPassenger(p);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player p = (Player) sender;
+        if (Objects.equals(command.toString(), "togglemobriding")) {
+            p.sendMessage("test");
+        }
+        return false;
     }
 }
