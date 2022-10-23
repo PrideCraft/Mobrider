92a1736f852f4d59cd39b63f18f45cea0d97d698
Notarin <424c414e4b@gmail.com>
2022-10-23 17:48:36 -0400

    Added a toggle to mobriding, controlled by a toggle command

--- src/main/java/me/notarin/mobrider/Mobrider.java
+++ src/main/java/me/notarin/mobrider/Mobrider.java
@@ -1,10 +1,25 @@
 package me.notarin.mobrider;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("SpellCheckingInspection")
 public final class Mobrider extends JavaPlugin {
+
+    static final List<String> toggles = new ArrayList<>();
 
     @Override
     public void onEnable() {
@@ -17,12 +32,12 @@ public final class Mobrider extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public class Events implements Listener {
+    public static class Events implements Listener {
         @EventHandler
         public void onInteract(PlayerInteractEntityEvent e) {
             Player p = e.getPlayer();
             Entity en = e.getRightClicked();
-            if(!(en instanceof ArmorStand) && !(en instanceof Player)) {
+            if(!(en instanceof ArmorStand) && !(en instanceof Player) && !(toggles.contains(String.valueOf(p)))) {
                 en.addPassenger(p);
             }
         }
@@ -31,8 +46,14 @@ public final class Mobrider extends JavaPlugin {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         Player p = (Player) sender;
-        if (Objects.equals(command.toString(), "togglemobriding")) {
-            p.sendMessage("test");
+        if (command.getName().equals("togglemobriding")) {
+            if (!toggles.contains(String.valueOf(p))) {
+                p.sendMessage(ChatColor.YELLOW + "Mobriding off");
+                toggles.add(String.valueOf(p));
+            } else {
+                p.sendMessage(ChatColor.YELLOW + "Mobriding on");
+                toggles.remove(String.valueOf(p));
+            }
         }
         return false;
     }
